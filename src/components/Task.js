29378b900fe846943f5a798e2a084aef7d28f784
Robert Nichols <rnichol2@cisco.com>
2020-11-12 12:02:59 -0500

    change task background to original

--- src/components/Task.js
+++ src/components/Task.js
@@ -21,7 +21,6 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
 					value={title} 
 					readOnly={true} 
 					placeholder="Input title" 
-					style={{ background: 'cyan' }}
 				/>
 			</div>
 			<div className="actions" onClick={event => event.stopPropagation()}>
